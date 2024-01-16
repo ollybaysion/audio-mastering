c47e84ad8cdc2ba6a3926a307c69e070ad4cb6ea
ds.kim <ollybaysion@gmail.com>
2024-01-16 21:35:50 +0900

    main 페이지

--- src/main/java/com/audiomaster/controller/ErrorController.java
+++ src/main/java/com/audiomaster/controller/ErrorController.java
@@ -6,7 +6,6 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@RequiredArgsConstructor
 @RequestMapping("error")
 @Controller
 public class ErrorController {
