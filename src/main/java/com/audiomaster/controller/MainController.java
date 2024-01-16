20e904da35067cb4403f23afee45b0895f620d4f
ds.kim <ollybaysion@gmail.com>
2024-01-16 21:33:35 +0900

    MainController 수정

--- src/main/java/com/audiomaster/controller/MainController.java
+++ src/main/java/com/audiomaster/controller/MainController.java
@@ -2,10 +2,12 @@ package com.audiomaster.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@RequestMapping("")
 @Controller
 public class MainController {
-    @GetMapping("/")
+    @GetMapping
     public String root() {
         return "/index";
     }
