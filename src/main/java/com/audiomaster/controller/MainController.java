f9e2ae0f4b0c7e85837b8cd1bbf18089ae0ef1e9
ds.kim <ollybaysion@gmail.com>
2024-01-16 21:43:23 +0900

    .

--- src/main/java/com/audiomaster/controller/MainController.java
+++ src/main/java/com/audiomaster/controller/MainController.java
@@ -10,6 +10,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class MainController {
     @GetMapping
     public String root(ModelMap map) {
-        return "/main";
+        return "main";
     }
 }
