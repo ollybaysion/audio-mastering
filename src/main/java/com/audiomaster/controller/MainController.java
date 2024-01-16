9513f7ad65310e1f5a91cafc5b8d5ef0e82f0f7e
ds.kim <ollybaysion@gmail.com>
2024-01-16 21:38:04 +0900

    Main

--- src/main/java/com/audiomaster/controller/MainController.java
+++ src/main/java/com/audiomaster/controller/MainController.java
@@ -1,6 +1,7 @@
 package com.audiomaster.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -8,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class MainController {
     @GetMapping
-    public String root() {
+    public String root(ModelMap map) {
         return "/main";
     }
 }
