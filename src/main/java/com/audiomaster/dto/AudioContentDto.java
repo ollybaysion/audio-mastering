a24211ceb3aad3878e3fd5e1be692f46acd58e1f
ds.kim <ollybaysion@gmail.com>
2024-01-26 14:13:29 +0900

    #12 - DTO
    - 파라미터 대신 processorWrapper로 변경

--- src/main/java/com/audiomaster/dto/AudioContentDto.java
+++ src/main/java/com/audiomaster/dto/AudioContentDto.java
@@ -1,26 +1,22 @@
 package com.audiomaster.dto;
 import com.audiomaster.audio.AudioWrapper;
-import com.audiomaster.audio.processor.compressor;
+import com.audiomaster.audio.processorWrapper;
 import org.springframework.web.multipart.MultipartFile;
 
 public record AudioContentDto(
         MultipartFile inputAudioFile,
-        double threshold,
-        double ratio,
-        double knee,
-        double attack,
-        double release,
+        processorWrapper proc,
         String outputFilename
 ){
-    public static AudioContentDto of(MultipartFile inputAudioFile, double threshold, double ratio, double knee, double attack, double release) {
-        return new AudioContentDto(inputAudioFile, threshold, ratio, knee, attack, release, null);
+    public static AudioContentDto of(MultipartFile inputAudioFile, processorWrapper proc) {
+        return new AudioContentDto(inputAudioFile, proc, null);
     }
 
     public static AudioContentDto of(String outputFilename) {
-        return new AudioContentDto(null, 0.0, 0.0, 0.0, 0.0, 0.0, outputFilename);
+        return new AudioContentDto(null, null, outputFilename);
     }
 
     public AudioWrapper toEntity() {
-        return new AudioWrapper(new compressor(threshold, ratio, knee, attack, release));
+        return new AudioWrapper(proc);
     }
 }
