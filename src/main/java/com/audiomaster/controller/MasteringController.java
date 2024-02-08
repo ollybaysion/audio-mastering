217099090a9f17215d1a82662b20fdecdfec42de
ds.kim <ollybaysion@gmail.com>
2024-02-08 18:47:39 +0900

    #16 - Controller
    - Service를 static으로 변경함에 따른 코드 변경
    - 일부 기타 변경

--- src/main/java/com/audiomaster/controller/MasteringController.java
+++ src/main/java/com/audiomaster/controller/MasteringController.java
@@ -1,8 +1,11 @@
 package com.audiomaster.controller;
 
+import com.audiomaster.audio.AudioEntity;
+import com.audiomaster.audio.ProcessorType;
 import com.audiomaster.dto.request.AudioFormRequest;
 import com.audiomaster.service.AudioMasteringService;
 import com.audiomaster.service.FileStore;
+import com.audiomaster.service.JniService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.UrlResource;
@@ -19,31 +22,33 @@ import java.net.MalformedURLException;
 public class MasteringController {
 
     private final AudioMasteringService audioMasteringService;
-    private final FileStore fileStore;
 
     @GetMapping
     public String mastering(ModelMap map) {
         map.addAttribute("processors", audioMasteringService.getProcessorList());
+        map.addAttribute("urls", audioMasteringService.getProcessorUrlList());
         return "mastering/main";
     }
 
     @GetMapping("form/{processorType}")
     public String formGet(@PathVariable String processorType, ModelMap map) {
         map.addAttribute("request", audioMasteringService.getRequest(processorType));
-        map.addAttribute("processorType", processorType);
-        map.addAttribute("params", audioMasteringService.getParams(processorType));
+        map.addAttribute("processorUrl", processorType);
+        map.addAttribute("params", audioMasteringService.getParams(ProcessorType.findByTypeUrl(processorType)));
         return "mastering/form";
     }
 
     @PostMapping("form/{processorType}")
-    public String formPost(@PathVariable String processorType, @ModelAttribute AudioFormRequest request, ModelMap map) throws IOException {
+    public String formPost(@ModelAttribute AudioFormRequest request) throws IOException {
+        FileStore.saveMultipartfile(request.getInputAudioFile(), "input.wav");
         audioMasteringService.process(request);
-        return "mastering/form";
+
+        return "mastering/result";
     }
 
     @ResponseBody
     @GetMapping("/audio/{filename}")
     public Resource getAudio(@PathVariable String filename, ModelMap map) throws MalformedURLException {
-        return new UrlResource("file:" + fileStore.getFullPath(filename));
+        return new UrlResource("file:" + FileStore.getFullPath(filename));
     }
 }
